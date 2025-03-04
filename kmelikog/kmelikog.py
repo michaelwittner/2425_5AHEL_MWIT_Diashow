e3d961b6bd229c6765a7dc4bdbc68163a43f72df
Kerim <kerimmelikoglu@gmail.com>
2025-03-04 14:47:34 +0100

    Update kmelikog.py
    Find out which codecs for which formats are supported
    Created VideoGenerator class for generating a video from images

--- kmelikog/kmelikog.py
+++ kmelikog/kmelikog.py
@@ -7,6 +7,7 @@ class VideoGenerator:
     def __init__(self):
         print("Video Generator")
 
+    # Definiere Methode
     def generate_video_from_images(self, images: tuple, filename: str, fps: int = 30, height: int = None,
                                    width: int = None, codec: str = "mp4v") -> None:
         """
@@ -20,6 +21,7 @@ class VideoGenerator:
         :param codec: Codec für das Erstellen des Videos (Standard: mp4v für MP4) (Optional)
         """
         print("Generating video...")
+        print("Codec:", codec)
 
         # Überprüfe, ob die Liste leer ist.
         if not images:
@@ -38,7 +40,7 @@ class VideoGenerator:
 
         # OpenCV VideoWriter initialisieren
         # Audiocodec definieren
-        fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Codec für MP4-Format
+        fourcc = cv2.VideoWriter_fourcc(*codec)  # Codec für MP4-Format
         # Initialisiere Video(-Writer)
         video = cv2.VideoWriter(filename, fourcc, fps, (widthVideo, heightVideo))
 
@@ -66,12 +68,21 @@ class VideoGenerator:
         print(f"Video erfolgreich erstellt: {filename}")
 
 # Main Programm
-def __main__():
+def main():
     # Definiere Bild Tuple mit Bilddauer (in Sekunden)
     image_list = (["bild1.jpg", 1], ["bild2.jpg", 2], ["bild3.jpg", 1], ["bild4.jpg", 1], ["bild5.jpg", 3], ["bild6.jpg", 1], ["bild7.jpg", 0.5], ["bild8.jpg", 1], ["bild9.jpg", 1], ["bild10.jpg", 1])  # Hier gibt man die Pfade der Bilder an, die zum Video konvertiert werden sollen
     # Generiere Video aus dem Tuple
     vg = VideoGenerator()
-    vg.generate_video_from_images(image_list, "output.mp4")
+    vg.generate_video_from_images(image_list, "output10.mp4")
+
+    # Folgende Codecs gelten für folgende Formate:
+    # Format: mp4 Codecs: mp4v, h264, avc1
+    # Format: avi Codecs: fmp4, xvid, mjpg, mp4v
+    # Format: mkv Codecs: h264, vp80, vp90, avc1
+    # Format: mov Codecs: h264, mjpg, mp4v
+    # Format: webm Codecs: vp80, vp90
+    # Format: flv Codecs: h264, flv1
+    # Format: wmv Codecs: wmv1, wmv2, wmv3
 
 if __name__ == "__main__":
-    __main__()
+    main()
