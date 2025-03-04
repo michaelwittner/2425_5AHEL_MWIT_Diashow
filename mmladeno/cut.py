0161d59ca5c5b595746f25c65651240fedb54522
mmladeno <mmladeno@htl-steyr.ac.at>
2025-03-04 14:52:07 +0100

    readme mmladeno

--- mmladeno/cut.py
+++ mmladeno/cut.py
@@ -1,16 +1,27 @@
 from pydub import AudioSegment
 
-print("test")
 
-# Öffnen einer MP3-Datei mit pydub.
-song = AudioSegment.from_mp3("WhoYouFoolin.mp3")
+def cut_audio(file_name, result_length, cut_direction):
+    # Öffnen der MP3-Datei
+    song = AudioSegment.from_mp3(file_name)
 
-# pydub arbeitet in millisekunden
-ten_seconds = 10 * 1000
+    # Umrechnung der gewünschten Länge in Millisekunden
+    result_length_ms = result_length * 1000
 
-# Startet bei 0 kann man aber auch selber definieren start_wert:end_wert
-first_10_seconds = song[:ten_seconds]
+    # Prüfen der Cut-Richtung
+    if cut_direction == "CUT-LEFT":
+        # Vom Anfang wegschneiden: Die letzten 'result_length' Sekunden behalten
+        result_audio = song[-result_length_ms:]
+    elif cut_direction == "CUT-RIGHT":
+        # Vom Ende wegschneiden: Die ersten 'result_length' Sekunden behalten
+        result_audio = song[:result_length_ms]
+    else:
+        raise ValueError("Bitte 'CUT-LEFT' oder 'CUT-RIGHT' angeben.")
 
-# save file
-first_10_seconds.export("first_10_seconds.mp3", format="mp3")
-print("New Audio file is created and saved")
+    # Speichern der neuen Datei
+    result_audio.export(f"cut_{cut_direction}_{result_length}_seconds.mp3", format="mp3")
+    print(f"Neue Datei '{f'cut_{cut_direction}_{result_length}_seconds.mp3'}' wurde erstellt und gespeichert.")
+
+
+# Beispielaufruf der Funktion
+cut_audio("WhoYouFoolin.mp3", 20,"CUT-LEFT")  # Beispiel für CUT-LEFT (erste 5 Sekunden entfernen, um 20 Sekunden zu erhalten)
