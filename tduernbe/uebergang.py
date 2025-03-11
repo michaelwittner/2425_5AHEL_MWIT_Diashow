7956f9e27aa4d16cbc38b501316c40087cb605ef
tduernbe <tduernbe@htl-steyr.ac.at>
2025-03-11 14:37:25 +0100

    Als Funktion definiert Parameter hinzugefügt einzelne Bilder werden ausgegeben Bilder werden nichtmehr angezeigt unnötige Codezeilen entfernt Ein bisschen Kommentiert

--- tduernbe/uebergang.py
+++ tduernbe/uebergang.py
@@ -1,24 +1,20 @@
 import cv2
 import numpy as np  #cv2 und numpy importiert
 
-image = cv2.imread(r"H:\MWIT\foto.jpg") #Bild einlesen
-height, width, _ = image.shape  #Abmessungen auslesen
 
-frames = 30 #Anzahl der Frames, die bearbeitet werden
-fps = 60
+def uebergang(bild_pfad, ziel_ordner, zeit_uebergang, fps):
 
-for i in range(frames + 1): #Schleife mit der Anzahl der Frames, sodass für jeden Frame geschwärzt wird
+    image = cv2.imread(bild_pfad) #Bild einlesen
+    height, width, _ = image.shape  #Abmessungen auslesen
 
-    alpha = 1 - (i / frames) #Alphawert für addWeighted berechnet, siehe Tutorial
-    darkened = cv2.addWeighted(image, alpha, np.zeros_like(image), 0, 0) #Bild nach und nach schwärzen
+    frames = int(fps * zeit_uebergang) # Frames berechnen weil Wittner sagt zeitübergang soll gewählt werden können
 
-    #https://docs.opencv.org/3.4/d5/dc4/tutorial_adding_images.html
+    for i in range(frames):
+        alpha = 1 - (i / frames)   #alphawert berechnen      https://stackoverflow.com/questions/1962795/how-to-get-alpha-value-of-a-png-image-with-pil
+        darkened = cv2.addWeighted(image, alpha, np.zeros_like(image), 0, 0)
+            # #https://docs.opencv.org/3.4/d5/dc4/tutorial_adding_images.html
 
-    cv2.imshow("Übergang", darkened) #Bild ausgeben/anzeigen
+        frame_name = ziel_ordner + "/blende_" + str(i + 1).zfill(3) + ".jpg"
+        cv2.imwrite(frame_name, darkened) # Datei speichern
 
-    if cv2.waitKey(5) & 0xFF == ord('q'): #bei waitKey kann man die Wiederholungsrate in ms eintragen, hier 1, weil der Übergang schnell sein soll
-        break
-
-cv2.waitKey(0)  # Wartet unendlich, bis eine Taste gedrückt wird.
-cv2.destroyAllWindows()
 
