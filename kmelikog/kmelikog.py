e2739a0c704ffe32ad5ae4b3d0fe4cc80a10f092
kmelikog <kerimmelikoglu@gmail.com>
2025-03-11 14:31:35 +0100

    Changes: It is possible to pass the directory in which the files are in.

--- kmelikog/kmelikog.py
+++ kmelikog/kmelikog.py
@@ -1,14 +1,27 @@
 import cv2
 from PIL import Image
+import os
 
-# VideoGenerator Klasse
+"""
+VideoGenerator Klasse
+description: Diese Klasse erstellt aus mehreren Bildern ein Video.
+usage: Erstelle ein TUPLE. In dieser Tuple wird pro Bild, das in ein Video
+hinzugefügt werden muss, eine Liste erstellt. Der 0. Index des Bildes ist der
+Dateienname und der 1. Index bestimmt, wie lang das Bild im Video erscheint, bevor
+das nächste Bild kommt. 
+
+Danach wird ein VideoGenerator Objekt erzeugt. (vg = VideoGenerator())
+Mit dem Objekt rufen wir die generate_video_from_images Methode auf. 
+Unten steht die Beschreibung dieser Methode und was man angeben kann/muss. 
+"""
+
 class VideoGenerator:
     # Konstruktor der VideoGenerator Klasse
     def __init__(self):
         print("Video Generator")
 
     def generate_video_from_images(self, images: tuple, filename: str, fps: int = 30, height: int = None,
-                                   width: int = None, codec: str = "mp4v") -> None:
+                                   width: int = None, codec: str = "mp4v", directory: str = ".") -> None:
         """
         Erstellt ein Video aus einer Liste von Bildern.
 
@@ -27,7 +40,7 @@ class VideoGenerator:
             return
 
         # Laden des ersten Bildes, um die Videogröße zu bestimmen
-        first_image = Image.open(images[0][0])
+        first_image = Image.open(os.path.join(directory, images[0][0]))
 
         # Überprüfe, ob height und width Parameter angegeben wurden
         if height is None and width is None:
@@ -45,7 +58,7 @@ class VideoGenerator:
         # for Schleife über die Bilder
         for img_path, duration in images:
             # Lade das Bild
-            img = cv2.imread(img_path)
+            img = cv2.imread(os.path.join(directory, img_path))
             # Überprüfe, ob Bild vorhanden ist
             if img is None:
                 print(f"Warnung: Bild {img_path} konnte nicht geladen werden und wird übersprungen.")
@@ -71,7 +84,7 @@ def __main__():
     image_list = (["bild1.jpg", 1], ["bild2.jpg", 2], ["bild3.jpg", 1], ["bild4.jpg", 1], ["bild5.jpg", 3], ["bild6.jpg", 1], ["bild7.jpg", 0.5], ["bild8.jpg", 1], ["bild9.jpg", 1], ["bild10.jpg", 1])  # Hier gibt man die Pfade der Bilder an, die zum Video konvertiert werden sollen
     # Generiere Video aus dem Tuple
     vg = VideoGenerator()
-    vg.generate_video_from_images(image_list, "output.mp4")
+    vg.generate_video_from_images(image_list, "output.mp4", directory=".\\proj")
 
 if __name__ == "__main__":
     __main__()
