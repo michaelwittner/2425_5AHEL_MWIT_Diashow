99c4f413cc18e95cef0a15bcd95403e0427d4301
sgramuel <sgramer@htl-steyr.ac.at>
2025-03-18 14:56:20 +0100

    Update diashow.py

--- sgramer/diashow.py
+++ sgramer/diashow.py
@@ -7,6 +7,7 @@ def slide_transition(image1_path, image2_path, output_folder, duration=1.5, fps=
     img1 = cv2.imread(image1_path)
     img2 = cv2.imread(image2_path)
 
+    # Prüfen ob gültige Bilder angegeben wurden
     if img1 is None or img2 is None:
         print("Fehler: Konnte ein Bild nicht laden.")
         return
@@ -15,13 +16,11 @@ def slide_transition(image1_path, image2_path, output_folder, duration=1.5, fps=
     height1, width1, _ = img1.shape
     height2, width2, _ = img2.shape
 
-    
     # Gemeinsame Größe festlegen (z. B. anhand des ersten Bildes)
     width = width1
     height = height1
     img2 = cv2.resize(img2, (width, height))
 
-    
     # Anzahl der Frames berechnen
     num_frames = int(duration * fps)
     frame_list = []  # Liste zum Speichern der Dateipfade und Anzeigedauer
@@ -31,28 +30,39 @@ def slide_transition(image1_path, image2_path, output_folder, duration=1.5, fps=
         os.makedirs(output_folder)
 
     # Überprüfen ob gültige Richtung eingegeben wurde
-    if direction not in ["SHIFT_LEFT", "SHIFT_RIGHT"]:
-        print("Fehler: Ungültige Richtung. Verwenden Sie 'SHIFT_LEFT' oder 'SHIFT_RIGHT'.")
+    if direction not in ["SHIFT_LEFT", "SHIFT_RIGHT", "SHIFT_UP", "SHIFT_DOWN"]:
+        print("Fehler: Ungültige Richtung. Verwenden Sie 'SHIFT_LEFT', 'SHIFT_RIGHT', 'SHIFT_UP' oder 'SHIFT_DOWN'.")
         return
 
     for i in range(num_frames + 1):
         alpha = i / num_frames  # Fortschritt von 0 bis 1
-        offset = int(alpha * width)  # Pixelverschiebung für den Übergang
+        offset_x = int(alpha * width)  # Pixelverschiebung für horizontale Bewegung
+        offset_y = int(alpha * height)  # Pixelverschiebung für vertikale Bewegung
 
         # Neue leere Leinwand in der gleichen Größe wie die Bilder erstellen
         frame = np.zeros((height, width, 3), dtype=np.uint8)
 
         # Bildbereiche für die Bewegung bestimmen
         if direction == "SHIFT_LEFT":
-            if offset < width:
-                frame[:, :width - offset] = img1[:, offset:]
-            if offset > 0:
-                frame[:, width - offset:] = img2[:, :offset]
+            if offset_x < width:
+                frame[:, :width - offset_x] = img1[:, offset_x:]
+            if offset_x > 0:
+                frame[:, width - offset_x:] = img2[:, :offset_x]
         elif direction == "SHIFT_RIGHT":
-            if offset < width:
-                frame[:, offset:] = img1[:, :width - offset]
-            if offset > 0:
-                frame[:, :offset] = img2[:, width - offset:]
+            if offset_x < width:
+                frame[:, offset_x:] = img1[:, :width - offset_x]
+            if offset_x > 0:
+                frame[:, :offset_x] = img2[:, width - offset_x:]
+        elif direction == "SHIFT_UP":
+            if offset_y < height:
+                frame[:height - offset_y, :] = img1[offset_y:, :]
+            if offset_y > 0:
+                frame[height - offset_y:, :] = img2[:offset_y, :]
+        elif direction == "SHIFT_DOWN":
+            if offset_y < height:
+                frame[offset_y:, :] = img1[:height - offset_y, :]
+            if offset_y > 0:
+                frame[:offset_y, :] = img2[height - offset_y:, :]
 
         # Bild speichern
         frame_path = os.path.join(output_folder, f"frame_{i:03d}.jpg")
@@ -60,8 +70,3 @@ def slide_transition(image1_path, image2_path, output_folder, duration=1.5, fps=
         frame_list.append(f"{frame_path}, {1}/{fps}")
 
     return frame_list
-
-# Funktion aufrufen
-frame_data = slide_transition("bild1.jpg", "bild2.jpg", ".\\Bilder", 1.5, 30, "SHIFT_RIGHT")
-
-print("\n".join(frame_data))
